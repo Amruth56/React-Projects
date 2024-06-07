29735c903fc74a57e8088c668aae2cb14fb48d89
Amruth <ammumandappa@gmail.com>
2024-06-07 22:54:36 +0530

    search box css

--- 01 Google Gemini 2.0/gemini-clone/src/components/main/main.jsx
+++ 01 Google Gemini 2.0/gemini-clone/src/components/main/main.jsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import './main.css'
-import {assets} from '../../assets/assets'
+import React from "react";
+import "./main.css";
+import { assets } from "../../assets/assets";
 
 const Main = () => {
   return (
-    <div className='main'>
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <p>How can i assist you ?</p>
         </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Dev</span></p>
-                <p>How can i assist you ?</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
-                      <img src={assets.compass_icon } alt="" />
-                </div>
-                <div className="card">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
-                      <img src={assets.bulb_icon } alt="" />
-                </div>
-                <div className="card">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eu   m?</p>
-                      <img src={assets.message_icon } alt="" />
-                </div>
-                <div className="card">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
-                      <img src={assets.code_icon } alt="" />
-                </div>
-            </div>
+        <div className="cards">
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              eum?
+            </p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              eum?
+            </p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              eu m?
+            </p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              eum?
+            </p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Search" />
+            <img src={assets.gallery_icon} alt="" />
+            <img src={assets.mic_icon} alt="" />
+            <img src={assets.send_icon} alt="" />
+          </div>
+        </div> 
+        <p className="bottom-info">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, delectus rerum dignissimos et harum amet atque tenetur quos impedit unde.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;

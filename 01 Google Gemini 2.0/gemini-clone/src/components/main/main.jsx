a7d4a23a8e03f2b3bdb29bacfd145a2d1f40a16d
Amruth <ammumandappa@gmail.com>
2024-06-07 21:42:21 +0530

    main container.

--- 01 Google Gemini 2.0/gemini-clone/src/components/main/main.jsx
+++ 01 Google Gemini 2.0/gemini-clone/src/components/main/main.jsx
@@ -9,6 +9,30 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt="" />
         </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How can i assist you ?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
+                      <img src={assets.compass_icon } alt="" />
+                </div>
+                <div className="card">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
+                      <img src={assets.bulb_icon } alt="" />
+                </div>
+                <div className="card">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eu   m?</p>
+                      <img src={assets.message_icon } alt="" />
+                </div>
+                <div className="card">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eum?</p>
+                      <img src={assets.code_icon } alt="" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }

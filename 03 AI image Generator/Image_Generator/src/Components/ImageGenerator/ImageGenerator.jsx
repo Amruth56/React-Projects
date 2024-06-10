5a4fa8886ad13f60273136a1f6a3e74aae242b0e
Amruth <ammumandappa@gmail.com>
2024-06-10 21:22:46 +0530

    .

--- 03 AI image Generator/Image_Generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ 03 AI image Generator/Image_Generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './ImageGenerator.css'
-import default_image from '../Assets/person.png'
+import default_image from '../Assets/minions.png'
 
 const ImageGenerator = () => {
   return (
     <div className = 'ai-image-generator'>
         <div className="header">AI image <span>Generator</span></div>
+        <div className="img-loading">
+          <div className="image">
+            <img src={default_image} alt="" />
+          </div>
+        </div>
     </div>
   )
 }

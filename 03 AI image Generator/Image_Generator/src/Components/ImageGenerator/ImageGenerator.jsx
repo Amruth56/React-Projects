c43f56de5cd85488bdf2a68fff7d08e510f9c640
Amruth <ammumandappa@gmail.com>
2024-06-11 12:24:34 +0530

    using OpenAI api key

--- 03 AI image Generator/Image_Generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ 03 AI image Generator/Image_Generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import './ImageGenerator.css'
-import default_image from '../Assets/minions.png'
+import React, { useState, useRef } from "react";
+import "./ImageGenerator.css";
+import default_image from "../Assets/minions.png";
 
 const ImageGenerator = () => {
-  return (
-    <div className = 'ai-image-generator'>
-        <div className="header">AI image <span>GENERATOR</span></div>
-        <div className="img-loading">
-          <div className="image">
-            <img src={default_image} alt="" />
-          </div>
-        </div>
-        <div className="search-box">
-          <input type=" " className="search-input" placeholder="Search" />
-          <div className="generate-btn">Generate</div>
-        </div>
-    </div>
-  )
-}
+  const [image_url, setImage_url] = useState("/");
+  let inputref = useRef(null);
 
-export default ImageGenerator
+  const imageGenerator = async () => {
+    if (inputref.current.value === "") {
+      return 0;
+    }
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer sk-proj-SM094Ox010Itilr84XVBT3BlbkFJTKGwaeElTDqXmdAwGnc7",
+          "User-Agent": "Chrome",
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="ai-image-generator">
+      <div className="header">
+        AI image <span>GENERATOR</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img src={image_url === "/" ? default_image : image_url} alt="" />
+        </div>
+      </div>
+      <div className="search-box">
+        <input
+          type="text "
+          ref={inputref}
+          className="search-input"
+          placeholder="Search"
+        />
+        <div className="generate-btn">Generate</div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGenerator;

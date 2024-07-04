c2724d3d8cfb4fc87b2729cabb22cf93ebd6f4cc
Amruth <ammumandappa@gmail.com>
2024-07-04 21:26:18 +0530

    .

--- 04 gify/src/components/header.jsx
+++ 04 gify/src/components/header.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
-    const [categories, setCategories] = useState([])
-    const [showCategories, setShowCategories] = useState(false)
+  const [categories, setCategories] = useState([]);
+  const [showCategories, setShowCategories] = useState(false);
   return (
     <nav>
-        <div>
-            <Link to='/'>
-            <img src="" alt=""></img></Link>
-        </div>
+      <div>
+        <Link to="/" className="flex gap-2">
+          <img src="/logo.svg" className="w-8" alt="gify logo"></img>
+          <h1 className="text-5xl font-bold tracking-tight cursor-pointer">GIFY</h1>
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

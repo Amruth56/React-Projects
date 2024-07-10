5c61ebf270b09defb175fc26c08668467da92acc
Amruth <ammumandappa@gmail.com>
2024-07-10 15:13:25 +0530

    .

--- 04accordion/src/components/index.jsx
+++ 04accordion/src/components/index.jsx
@@ -4,19 +4,27 @@ import data from "./data";
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
 
-  function handleSingleSelection(getCurrentId){
-
+  function handleSingleSelection(getCurrentId) {
+    // console.log(getCurrentId);
+    setSelected(getCurrentId === selected ? null :getCurrentId );
   }
+  console.log(selected);
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem, index) => (
             <div className="item" key={index}>
-              <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (

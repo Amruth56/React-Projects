06978e095f512baa6289e099f3796eda6799d9d7
Amruth <ammumandappa@gmail.com>
2024-07-12 08:52:25 +0530

    drop down menu

--- 05swiggyClone/src/App.jsx
+++ 05swiggyClone/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return <div className="text-3xl text-[#3498db] font-bold  ">
+  return <div className="   ">
     <Header></Header>
   </div>;
 }
--- 05swiggyClone/src/components/Header.jsx
+++ 05swiggyClone/src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { RxCaretDown } from "react-icons/rx";
+
 
 const Header = () => {
   return (
-    <header className="p-11 shadow-xl">
-      <div className="max-w-[1200px] mx-auto border border-red-200">
+    <header className="p-[15px] shadow-xl">
+      <div className="max-w-[1200px] mx-auto border border-red-200 flex items-center">
         <div className="w-[100px]">
-          <img src="images/logo.png" alt ="swiggy logo" className="w-full"></img>
+          <img src="images/logo.png" alt="swiggy logo" className="w-full"></img>
+        </div>
+        <div className="">
+          {" "}
+          <span className="font-bold border-b-[3px] border-b-black">
+            Amruth Mandappa
+          </span>
+          , Karnataka, India <RxCaretDown  className="inline text-[0.9rem] text-[#fc8019] "/>
+
         </div>
       </div>
     </header>

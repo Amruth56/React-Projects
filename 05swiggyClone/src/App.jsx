5afe90b829f5c26473f4965418a10e8388645c97
Amruth <ammumandappa@gmail.com>
2024-07-12 08:36:52 +0530

    .

--- 05swiggyClone/src/App.jsx
+++ 05swiggyClone/src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="text-3xl text-[#3498db] font-bold  ">hi hello</div>;
+  return <div className="text-3xl text-[#3498db] font-bold  ">
+    <Header></Header>
+  </div>;
 }
 
 export default App;
---
+++ 05swiggyClone/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="p-11 shadow-xl">
+    <div className="max-w-[1200px] mx-auto border border-red-200">home</div>
+  </header>;
+};
+
+export default Header;

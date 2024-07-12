b879d74214a8929296aec54a8a0c247f98ec7ee0
Amruth <ammumandappa@gmail.com>
2024-07-12 08:42:06 +0530

    .

--- 05swiggyClone/src/components/Header.jsx
+++ 05swiggyClone/src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="p-11 shadow-xl">
       <div className="max-w-[1200px] mx-auto border border-red-200">
-        <div>
-          <img src="images/logo.png" alt ="swiggy logo"></img>
+        <div className="w-[100px]">
+          <img src="images/logo.png" alt ="swiggy logo" className="w-full"></img>
         </div>
       </div>
     </header>

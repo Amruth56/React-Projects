e1ee5539ea1773f1a7bf5fc0ff920cfab761bef4
Amruth <ammumandappa@gmail.com>
2024-07-12 10:38:46 +0530

    .

--- 05swiggyClone/src/components/Header.jsx
+++ 05swiggyClone/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { RxCaretDown } from "react-icons/rx";
+import { useState } from "react";
 
 
 const Header = () => {
+    // const [toggle, setToggle] useState(false)
   return (
-    <header className="p-[15px] shadow-xl">
+    <>
+    <div className="black-overlay w-full h-full fixed">
+    <header className="p-[15px] shadow-xl text-[#686b78]">
       <div className="max-w-[1200px] mx-auto border border-red-200 flex items-center">
         <div className="w-[100px]">
           <img src="images/logo.png" alt="swiggy logo" className="w-full"></img>
@@ -14,11 +18,13 @@ const Header = () => {
           <span className="font-bold border-b-[3px] border-b-black">
             Amruth Mandappa
           </span>
-          , Karnataka, India <RxCaretDown  className="inline text-[0.9rem] text-[#fc8019] "/>
+          , Karnataka, India <RxCaretDown fontSize={25} className="  inline  text-[#fc8019] cursor-pointer "/>
 
         </div>
       </div>
     </header>
+    </div>
+    </>
   );
 };
 

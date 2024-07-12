5e4a74f02ed0abfa2d2fa189b37141a8a2966cbe
Amruth <ammumandappa@gmail.com>
2024-07-12 08:39:46 +0530

    .

--- 05swiggyClone/src/components/Header.jsx
+++ 05swiggyClone/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="p-11 shadow-xl">
-    <div className="max-w-[1200px] mx-auto border border-red-200">home</div>
-  </header>;
+  return (
+    <header className="p-11 shadow-xl">
+      <div className="max-w-[1200px] mx-auto border border-red-200">
+        <div>
+          <img src="images/logo.png" alt ="swiggy logo"></img>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

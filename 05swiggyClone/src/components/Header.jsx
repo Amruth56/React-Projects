0b85f2853c0d1796d9fce0eb084370257d0dfdb0
Amruth <ammumandappa@gmail.com>
2024-07-12 11:01:06 +0530

    opacity and visibility.

--- 05swiggyClone/src/components/Header.jsx
+++ 05swiggyClone/src/components/Header.jsx
@@ -2,28 +2,43 @@ import React from "react";
 import { RxCaretDown } from "react-icons/rx";
 import { useState } from "react";
 
-
 const Header = () => {
-    // const [toggle, setToggle] useState(false)
+  const [toggle, setToggle] = useState(false);
+
+  const showSideMenu = () => {
+    console.log("hiii");
+    setToggle(true);
+  };
   return (
     <>
-    <div className="black-overlay w-full h-full fixed">
-    <header className="p-[15px] shadow-xl text-[#686b78]">
-      <div className="max-w-[1200px] mx-auto border border-red-200 flex items-center">
-        <div className="w-[100px]">
-          <img src="images/logo.png" alt="swiggy logo" className="w-full"></img>
+      <div
+        className="black-overlay w-full h-full fixed duration-500"
+        style={{ opacity: toggle ? 1 : 0,
+            visibility: toggle ? 'visible' : 'hidden' }}
+         
+      ></div>
+      <header className="p-[15px] shadow-xl text-[#686b78]">
+        <div className="max-w-[1200px] mx-auto border border-red-200 flex items-center">
+          <div className="w-[100px]">
+            <img
+              src="images/logo.png"
+              alt="swiggy logo"
+              className="w-full"
+            ></img>
+          </div>
+          <div className="">
+            <span className="font-bold border-b-[3px] border-b-black">
+              Amruth Mandappa
+            </span>
+            , Karnataka, India
+            <RxCaretDown
+              onClick={showSideMenu}
+              fontSize={25}
+              className="  inline  text-[#fc8019] cursor-pointer"
+            />
+          </div>
         </div>
-        <div className="">
-          {" "}
-          <span className="font-bold border-b-[3px] border-b-black">
-            Amruth Mandappa
-          </span>
-          , Karnataka, India <RxCaretDown fontSize={25} className="  inline  text-[#fc8019] cursor-pointer "/>
-
-        </div>
-      </div>
-    </header>
-    </div>
+      </header>
     </>
   );
 };

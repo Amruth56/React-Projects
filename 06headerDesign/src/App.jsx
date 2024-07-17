0b8cbd9305c3af00e9cc83a002df79a32723d910
Amruth <ammumandappa@gmail.com>
2024-07-17 23:25:36 +0530

    .

--- 06headerDesign/src/App.jsx
+++ 06headerDesign/src/App.jsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
+  let heroData = [
+    { text1: "Dive into", text2: "What you love" },
+    { text1: "Indulge", text2: "Your Passions" },
+    { text1: "Give Into", text2: "Your Passions" },
+  ];
 
-  return (
-    
-    <>hi</>
-  )
+  return <>hi</>;
 }
 
-export default App
+export default App;

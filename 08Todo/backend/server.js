316c0cf70ebd6ce258c1fbb88cc0fa48f6afbcd8
Amruth <ammumandappa@gmail.com>
2025-12-25 23:52:59 +0530

    designed the schema

--- 08Todo/backend/server.js
+++ 08Todo/backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server is running...");
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+import "./src/config/redis.js";
+
+connectDB();
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
---
+++ 08Todo/backend/src/models/todo.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low",
+    },
+    dueDate: Date,
+  },
+  { timestamps: true }
+);
